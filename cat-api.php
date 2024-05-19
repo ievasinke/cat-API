8143678a0d00bc9a458617ab56871e6ecc4a886c
Ieva Šinke <ieva.sinke@gmail.com>
2024-05-19 15:19:48 +0300

    random fact

--- cat-api.php
+++ cat-api.php
@@ -2,6 +2,5 @@
 // Display CAT facts upon launching the application
 $catFactsData = file_get_contents("https://cat-fact.herokuapp.com/facts");
 $catFacts = json_decode($catFactsData);
-foreach ($catFacts as $catFact) {
-    echo "😻 $catFact->text\n";
-}
+$catFact = $catFacts[array_rand($catFacts)];
+echo "😻 $catFact->text\n";
