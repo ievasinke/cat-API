52b547b4de5f7a41bc45f53fdd8cc14e03b57c1b
Ieva Šinke <ieva.sinke@gmail.com>
2024-05-20 18:47:02 +0300

    data validation

--- cat-api.php
+++ cat-api.php
@@ -2,5 +2,10 @@
 // Display CAT facts upon launching the application
 $catFactsData = file_get_contents("https://cat-fact.herokuapp.com/facts");
 $catFacts = json_decode($catFactsData);
+
+if (empty($catFacts)) {
+    exit("Failed to fetch data. Please try again later.\n");
+}
+
 $catFact = $catFacts[array_rand($catFacts)];
 echo "😻 $catFact->text\n";
